e80f9b67f368ebf234a9013d511bc108ccda2328
WindSekirun <windsekirun@live.co.kr>
2015-08-05 23:01:40 +0900

    implement Clickable class, OnLinkClickListener

---
+++ library/src/main/java/com/github/windsekirun/naraetextview/Clickable.java
@@ -0,0 +1,56 @@
+package com.github.windsekirun.naraetextview;
+
+import java.util.regex.Pattern;
+
+/**
+ * NaraeTextView
+ * class: Clickable
+ * Created by WindSekirun on 15. 8. 5..
+ */
+public class Clickable {
+
+    protected String text;
+    protected Pattern pattern;
+
+    protected OnLinkClickListener linkClickListener;
+
+    public Clickable(Clickable clickable) {
+        text = clickable.text;
+        pattern = clickable.pattern;
+        linkClickListener = clickable.linkClickListener;
+    }
+
+    public Clickable(String text) {
+        this.text = text;
+        this.pattern = null;
+    }
+
+    public Clickable(Pattern pattern) {
+        this.pattern = pattern;
+        this.text = null;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public OnLinkClickListener getLinkClickListener() {
+        return linkClickListener;
+    }
+
+    public void setLinkClickListener(OnLinkClickListener linkClickListener) {
+        this.linkClickListener = linkClickListener;
+    }
+}
---
+++ library/src/main/java/com/github/windsekirun/naraetextview/OnLinkClickListener.java
@@ -0,0 +1,11 @@
+package com.github.windsekirun.naraetextview;
+
+/**
+ * NaraeTextView
+ * class: OnLinkClickListener
+ * Created by WindSekirun on 15. 8. 5..
+ */
+public interface OnLinkClickListener {
+
+    void onClick(String text);
+}
