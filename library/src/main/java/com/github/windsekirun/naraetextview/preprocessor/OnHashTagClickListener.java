828c9b08b710d7f981a89274e9013195383431ed
WindSekirun <windsekirun@live.co.kr>
2015-08-05 23:51:18 +0900

    delete useless class

--- library/src/main/java/com/github/windsekirun/naraetextview/preprocessor/OnHashTagClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.github.windsekirun.naraetextview.preprocessor;
-
-import com.github.windsekirun.naraetextview.util.OnLinkClickListener;
-
-/**
- * Created by WindSekirun on 15. 8. 5..
- */
-
